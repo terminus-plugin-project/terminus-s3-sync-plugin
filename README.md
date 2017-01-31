ad9d7783f737eb2666963ef333d77f1c07e0adf9
Sean Dietrich <sean.dietrich@inresonance.com>
2017-01-30 16:09:56 -0800

    Reworked Sync to stream downloads and stream uploads to AWS

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Terminus S3 Sync Plugin
-Plugin to sync a site's backups with an AWS S3 Bucket
+
+[![Terminus v1.x Compatible](https://img.shields.io/badge/terminus-v1.x-green.svg)](https://github.com/terminus-plugin-project/terminus-s3-sync-plugin)
+
+Terminus Plugin that sync a site's backups with an AWS S3 Bucket
 
 ## Installation
 
+For help installing, see Manage Plugins]
+
+```
+mkdir -p ~/.terminus/plugins
+composer create-project -d ~/.terminus/plugins terminus-plugin-project/terminus-s3-sync-plugin:~1
+```
+
 ## Help
+
+Run `terminus help s3-sync` for help.
--- src/Commands/SyncS3Command.php
+++ src/Commands/SyncS3Command.php
@@ -5,12 +5,16 @@ namespace Pantheon\TerminusS3Sync\Commands;
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Psr7;
 use Pantheon\Terminus\Commands\TerminusCommand;
 use Pantheon\Terminus\Request\RequestAwareInterface;
 use Pantheon\Terminus\Request\RequestAwareTrait;
 use Pantheon\Terminus\Site\SiteAwareInterface;
 use Pantheon\Terminus\Site\SiteAwareTrait;
-use Robo\Common\IO;
+use League\Container\ContainerAwareInterface;
+use League\Container\ContainerAwareTrait;
+use WriteiniFile\WriteiniFile;
+use GuzzleHttp\Promise\EachPromise;
 
 use Aws\S3\S3Client;
 
@@ -18,10 +22,10 @@ use Aws\S3\S3Client;
  * Class SyncS3Command
  * Syncs a sites backups with an S3 Account
  */
-class SyncS3Command extends TerminusCommand implements SiteAwareInterface, RequestAwareInterface
-{
+class SyncS3Command extends TerminusCommand implements SiteAwareInterface, RequestAwareInterface, ContainerAwareInterface {
   use RequestAwareTrait;
   use SiteAwareTrait;
+  use ContainerAwareTrait;
 
   /**
    * Syncs a sites backup files with S3 bucket
@@ -39,72 +43,205 @@ class SyncS3Command extends TerminusCommand implements SiteAwareInterface, Reque
    * @option string $aws_secret AWS Secret (optional)
    * @option string $tmp_location Temporary Location to store downloads (optional)
    * @option string $save_path Path within AWS bucket to store downloads (optional)
+   * @option boolean $save Save config settings (optional)
+   * @option int $concurrent Concurrent Downloads to process (optional)
+   * @option string $elements Elements to sync (optional)
+   * @option string $after Date which to start syncing items (optional)
    *
-   * @usage terminus site:sync-s3 <site>.<env> <bucket> <profile> <aws_region> <aws_key> <aws_secret> <tmp_location> <bucket_path>
+   * @usage terminus site:sync-s3 <site>.<env> <bucket> <profile> <aws_region> <aws_key> <aws_secret> <tmp_location> <save_path> <save> <concurrent> <elements> <after>
    */
-  public function sync($site_env, $bucket, $options = ['profile' => NULL, 'aws_region' => NULL, 'aws_key' => NULL, 'aws_secret' => NULL, 'tmp_location' => NULL, 'save_path' => NULL]) {
+  public function sync($site_env, $bucket, $options = [
+    'profile' => NULL,
+    'aws_region' => NULL,
+    'aws_key' => NULL,
+    'aws_secret' => NULL,
+    'tmp_location' => '/tmp',
+    'save_path' => NULL,
+    'save' => FALSE,
+    'concurrent' => 2,
+    'elements' => NULL,
+    'after' => NULL
+  ]) {
     list($site, $env) = $this->getOptionalSiteEnv($site_env);
 
     $client_options = $this->generateCredentials($options);
     $client = new S3Client($client_options);
     $client->registerStreamWrapper();
 
-    $dir = "s3://" . $bucket . '/' ;
+    $dir = '';
 
-    if(!empty($options['save_path']))
+    if (!empty($options['save_path'])) {
       $dir .= $options['save_path'] . '/';
+    }
 
-    $envs = array();
-    if(is_null($env)) {
+    /**
+     * Get all environments for a site.
+     */
+    $envs = [];
+    if (is_null($env)) {
       $envs = $site->getEnvironments()->ids();
-    }else{
+    }
+    else {
       $envs[] = $env->id;
     }
 
-    $backup_element = NULL;
-    foreach($envs AS $tmp_env) {
+    /**
+     * Create location if does not exist.
+     */
+    if (!file_exists($options['tmp_location'])) {
+      mkdir($options['tmp_location'], 0777);
+    }
+
+    foreach ($envs AS $tmp_env) {
       $tmp = $site->get('name') . '.' . $tmp_env;
       list(, $full_env) = $this->getSiteEnv($tmp);
-      $tmp_dir = $dir . $site->get('name') . '/' . $full_env->id . '/';
-      if(!file_exists($tmp_dir)) {
-        mkdir($tmp_dir, 0777, TRUE);
+      $bucket_dir = $dir . $site->get('name') . '/' . $full_env->id . '/';
+      if (!file_exists("s3://" . $bucket . '/' . $bucket_dir)) {
+        //mkdir("s3://" . $bucket . '/' . $bucket_dir, 0777, TRUE);
       }
 
-      $backups = $full_env->getBackups()->getFinishedBackups($backup_element);
-      foreach ($backups AS $backup_key => $backup) {
-        $file_name = $tmp_dir . $backup->get('filename');
-        if(!file_exists($file_name)) {
-          //file_put_contents($file_name, 'a');
+
+      $backups = $full_env->getBackups()
+        ->getFinishedBackups($options['elements']);
+      // Reference: https://blog.madewithlove.be/post/concurrent-http-requests/
+
+      // @todo: Check to see if file already exists otherwise do not create an upload.
+      $promises = (function () use ($backups, $client, $bucket, $bucket_dir, $options) {
+
+        foreach ($backups AS $backup_key => $backup) {
+          $file_name = $bucket_dir . $backup->get('filename');
+          if(file_exists("s3://" . $bucket . '/' . $file_name)){
+            continue;
+          }
+
+          $backup_url = $backup->getUrl();
+
+          $file = fopen($backup_url, 'r');
+          $original = Psr7\stream_for($file);
+          $stream = new Psr7\CachingStream($original);
+          yield $client->putObjectAsync(array(
+            'Bucket' => $bucket,
+            'Key' => $file_name,
+            'Body' => $stream,
+            '@http' => [
+              'progress' => function ($expectedDl, $dl, $expectedUl, $ul) use ($file_name, $options){
+                if($options['verbose']) {
+                  printf(
+                    "%s: %s/%s %s uploaded\n",
+                    basename($file_name),
+                    $this->formatBytes($ul),
+                    $this->formatBytes($expectedUl),
+                    $this->formatePercentage($ul, $expectedUl )
+                  );
+                }
+            }],
+          ))->then(function() use($file_name){
+            print $file_name . ": Complete\n";
+          });
         }
-        print_r($backup->getDate());
-        die;
+      })();
 
-        $backup_url = $backup->getUrl();
-        $save_path = '/tmp/' . $backup->get('filename');
-        //Download file locally
-        //$this->request()->download($backup_url, $save_path);
-      }
+      $waiter = new EachPromise($promises, [
+        'concurrency' => $options['concurrent'],
+        'fulfilled' => function () {
+
+        },
+      ]);
+
+      $waiter->promise()->wait();
     }
   }
 
-  private function generateCredentials($options){
+  private function formatePercentage($a, $b){
+    return number_format( ($a / $b) * 100, 2 ) . '%';
+  }
+
+  private function generateCredentials($options) {
     $data = [
       'version' => 'latest',
       'region' => 'us-east-1'
     ];
 
-    if(!empty($options['aws_region']))
+    if (!empty($options['aws_region'])) {
       $data['region'] = $options['aws_region'];
+    }
 
-    if(!empty($options['profile']))
+    if (!empty($options['profile'])) {
       $data['profile'] = $options['profile'];
+    }
 
-    if(!empty($options['aws_key']) && !empty($options['aws_secret'])) {
+    if (!empty($options['aws_key']) && !empty($options['aws_secret'])) {
       $data['credentials']['key'] = $options['aws_key'];
       $data['credentials']['secret'] = $options['aws_secret'];
     }
 
     return $data;
+  }
+
+  /**
+   * Configure S3 Credentials
+   *
+   * @command site:sync-s3:config
+   * @aliases site:s3:config
+   *
+   * @option string $profile AWS Profile (optional)
+   * @option string $aws_region AWS Region (optional)
+   * @option string $aws_key AWS Key (optional)
+   * @option string $aws_secret AWS Secret (optional)
+   *
+   * @usage terminus site:sync-s3:config <profile> <aws_region> <aws_key> <aws_secret>
+   */
+  public function s3Config($options = [
+    'profile' => 'default',
+    'aws_region' => 'us-east-1',
+    'aws_key' => NULL,
+    'aws_secret' => NULL
+  ]) {
+    $home = $_SERVER['HOME'];
+
+    if (!file_exists($home . '/.aws')) {
+      mkdir($home . '/.aws');
+    }
+
+    $data_credentials = array();
+    if (file_exists($home . '/.aws/credentials')) {
+      $data_credentials = parse_ini_file($home . '/.aws/credentials', TRUE);
+    }
+
+    $data_credentials[$options['profile']] = array(
+      'aws_access_key_id' => $options['aws_key'],
+      'aws_secret_access_key' => $options['aws_secret']
+    );
+
+    chmod($home . '/.aws/credentials', 0777);
+    $credentials = new WriteiniFile($home . '/.aws/credentials');
+    $credentials->update($data_credentials);
+    $credentials->write();
+
+
+    $data_config = array();
+    if (file_exists($home . '/.aws/config')) {
+      $data_config = parse_ini_file($home . '/.aws/config', TRUE);
+    }
+    $data_config[$options['profile']] = array(
+      'region' => $options['aws_region'],
+      'output' => 'json'
+    );
+    chmod($home . '/.aws/config', 0777);
+    $config = new WriteiniFile($home . '/.aws/config');
+    $config->update($data_config);
+    $config->write();
+  }
+
+  /**
+   * @param int $bytes Number of bytes (eg. 25907)
+   * @param int $precision [optional] Number of digits after the decimal point (eg. 1)
+   * @return string Value converted with unit (eg. 25.3KB)
+   */
+  private function formatBytes($bytes, $precision = 2) {
+    $unit = ["B", "KB", "MB", "GB"];
+    $exp = floor(log($bytes, 1024)) | 0;
+    return round($bytes / (pow(1024, $exp)), $precision).$unit[$exp];
   }
 }
 
